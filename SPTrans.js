32cdf5a9ca18a27bbdb29630f087bd08e4e94e5c
Bruno Murozaki <brunoimurozaki@gmail.com.com>
2018-07-09 20:49:13 -0300

    commenting the disable part

--- SPTrans.js
+++ SPTrans.js
@@ -5,8 +5,14 @@ function loginSPTrans(){
     var chave = "351cf23405f3af731712cbace009b8966406e073098e80b107a519132a7ba50c";
     var url = "http://api.olhovivo.sptrans.com.br/v2.1/Login/Autenticar?token=" + chave;
 
-    $.post(url, function(data){
-        console.log(data);
+    $.ajax({
+        url: url,
+        crossDomain: true,
+        data: form,
+        dataType: 'json',
+        success: function(data) {
+            console.log(data);
+        },
+        type: 'POST'
     });
-
 }
