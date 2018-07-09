851eebce02872563847c1941fb129ee9cfa16ea8
Bruno Murozaki <brunoimurozaki@gmail.com.com>
2018-07-09 19:55:36 -0300

    Merging the testehtml to the index.php

--- index.php
+++ index.php
@@ -22,37 +22,6 @@
         $result = curl_exec ($ch); 
         curl_close ($ch);
         //echo "<hr>";
-
-        $ch = curl_init(); 
-        curl_setopt ($ch, CURLOPT_URL, $url . '/Linha/Buscar?termosBusca=Pinheiros'); 
-        curl_setopt ($ch, CURLOPT_POST, false); 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Length: 0'));
-        curl_setopt ($ch, CURLOPT_COOKIEFILE, $cookie); 
-        curl_setopt ($ch, CURLOPT_COOKIEJAR, $cookie); 
-        $result = curl_exec ($ch); 
-        curl_close ($ch);
-        //echo "<hr>";
-
-        exit;
-        $ch = curl_init(); 
-        curl_setopt ($ch, CURLOPT_URL, $url . '/Posicao/Linha?codigoLinha=34414'); 
-        curl_setopt ($ch, CURLOPT_POST, false); 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Length: 0'));
-        curl_setopt ($ch, CURLOPT_COOKIEFILE, $cookie); 
-        curl_setopt ($ch, CURLOPT_COOKIEJAR, $cookie); 
-        $result = curl_exec ($ch); 
-        curl_close ($ch);
-        //echo "<hr>";
-
-        $url = $url . "/Posicao/Linha?codigoLinha=34414";
-        $response = \Httpful\Request::get($url)
-            ->expectsJson()
-            ->withXTrivialHeader('Content-Length: 0')
-            ->addOnCurlOption(CURLOPT_COOKIEJAR, $cookie)
-            ->addOnCurlOption(CURLOPT_COOKIEFILE, $cookie)
-            ->send();
-
-        print_r($response);
     ?>
 
     <head>
